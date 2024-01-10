2be345a686f1ef27c2608f0deecc3fff2b3ab030
Gosia Magdzik <melassa26@gmail.com>
2024-01-10 19:50:47 +0100

    random f. added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-
+import { useState } from "react";
+import words from "./wordList.json";
 
 function App() {
+
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    words[Math.floor(Math.random() * words.length)]
+  });
 
   return (
     <h1>
