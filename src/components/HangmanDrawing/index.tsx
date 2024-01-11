568c45e8a074f81b7da2edc77443a0e6c737ed76
Gosia Magdzik <melassa26@gmail.com>
2024-01-11 16:10:18 +0100

    hangman styling

--- src/components/HangmanDrawing/index.tsx
+++ src/components/HangmanDrawing/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Bottom, 
+  DrawingWrapper,
+  Vertical, 
+  Top,
+  DropDown,
+  HeadStyled,
+  BodyStyled,
+  RightArmStyled,
+  LeftArmStyled,
+  RightLegStyled,
+  LeftLegStyled
+} from './styled';
+
+const Head = <HeadStyled/>
+const Body = <BodyStyled/>
+const RightArm = <RightArmStyled/>
+const LeftArm = <LeftArmStyled/>
+const RightLeg = <RightLegStyled/>
+const LeftLeg = <LeftLegStyled/>
 
 export const HangmanDrawing = () => {
   return (
     <DrawingWrapper>
-      HangmanDrawing
+        {Head}
+        {Body}
+        {RightArm}
+        {LeftArm}
+        {RightLeg}
+        {LeftLeg}
+      <DropDown/>
+      <Top/>
+      <Vertical/>
+      <Bottom/>
     </DrawingWrapper>
   )
 }
--- src/components/HangmanDrawing/styled.tsx
+++ src/components/HangmanDrawing/styled.tsx
@@ -1,1 +1,93 @@
 import styled from "styled-components";
+
+export const DrawingWrapper = styled.div`
+    position: relative;
+`
+
+export const Bottom = styled.div`
+    height: 1rem;
+    width: 16rem;
+    background-color: black;
+`
+
+export const Vertical = styled.div`
+    height: 25rem;
+    width: 1rem;
+    background-color: black;
+    margin-left: 8rem;
+`
+
+export const Top = styled.div`
+    height: 1rem;
+    width: 14rem;
+    background-color: black;
+    margin-left: 8rem;
+`
+
+export const DropDown = styled.div`
+    height: 3rem;
+    width: 1rem;
+    background-color: black;
+    position: absolute;
+    top: 0;
+    right: 0;
+`
+
+export const HeadStyled = styled.div`
+    width: 4rem;
+    height: 4rem;
+    border-radius: 100%;
+    border: 0.5rem solid black;
+    position: absolute;
+    top: 3rem;
+    right: -1.5rem;
+`
+
+export const BodyStyled = styled.div`
+    width: 0.7rem;
+    height: 6rem;
+    position: absolute;
+    top: 7rem;
+    right: 0rem;
+    background-color: black;
+`
+
+export const RightArmStyled = styled.div`
+    width: 6rem;
+    height: 0.4rem;
+    position: absolute;
+    top: 8rem;
+    right: -5.4rem;
+    background-color: black;
+    transform: rotate(2.5rad);
+`
+
+export const LeftArmStyled = styled.div`
+    width: 6rem;
+    height: 0.4rem;
+    position: absolute;
+    top: 8rem;
+    right: 0.08rem;
+    background-color: black;
+    transform: rotate(-2.5rad);
+`
+
+export const RightLegStyled = styled.div`
+    width: 6rem;
+    height: 0.5rem;
+    position: absolute;
+    top: 14.2rem;
+    right: -5.4rem;
+    background-color: black;
+    transform: rotate(-2.5rad);
+`
+
+export const LeftLegStyled = styled.div`
+    width: 6rem;
+    height: 0.5rem;
+    position: absolute;
+    top: 14.2rem;
+    right: 0.08rem;
+    background-color: black;
+    transform: rotate(2.5rad);
+`
