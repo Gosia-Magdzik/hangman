0de29641c0d40e735e06cb7d6a980692e8c9a239
Gosia Magdzik <melassa26@gmail.com>
2024-01-11 18:40:02 +0100

    error handling

--- src/components/HangmanWord/index.tsx
+++ src/components/HangmanWord/index.tsx
@@ -3,24 +3,24 @@ import { WordWrapper,
 } from './styled';
 
 const word = "test";
-const guessedLetters = ["t", "z"]
+const guessedLetter = ["t", "z"]
 
 export const HangmanWord = () => {
   return (
     <WordWrapper>
-      {word.split("").map((letter, index) =>
+      {word.split("").map((letter, index) => (
         <LetterStyling key={index}>
           <span
             style={{
-              visibility: guessedLetters.includes(letter)
+              visibility: guessedLetter.includes(letter)
               ? "visible"
-              : "hidden"
+              : "hidden",
             }}
           >
             {letter}
           </span>
         </LetterStyling>
-      )}
+      ))}
     </WordWrapper>
   )
 }
