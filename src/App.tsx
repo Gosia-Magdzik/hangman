4d0101d8de9dd9571e2b12ae7458c18555ba8314
Gosia Magdzik <melassa26@gmail.com>
2024-01-14 17:22:20 +0100

    preventing before over clicking on keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,9 @@ function App() {
   const isWinner = wordToGuess.split("").every(letter => guessedLetters.includes(letter))
 
   const addGuessedLetter = useCallback((letter: string) => {
-    if(guessedLetters.includes(letter)) return
+    if(guessedLetters.includes(letter) || isLoser || isWinner) return
     setGuessedLetters(currentLetters => [...currentLetters, letter])
-  }, [guessedLetters])
+  }, [guessedLetters, isWinner, isLoser])
 
 
 
